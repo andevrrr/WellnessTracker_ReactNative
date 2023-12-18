ed966e590e64675dfb1e59253f689b38c26c0e92
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-18 16:07:45 +0300

    navigation tap is ready

--- src/navigation/TapNavigator.js
+++ src/navigation/TapNavigator.js
@@ -3,11 +3,52 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ScheduleScreen from '../screens/ScheduleScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { View } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'Schedule') {
+          iconName = focused ? 'calendar' : 'calendar-outline';
+        } else if (route.name === 'Settings') {
+          iconName = focused ? 'settings' : 'settings-outline';
+        }
+
+        return (
+          <View style={{ top: 15 }}>
+            <Ionicons name={iconName} size={size} color={color} />
+          </View>
+        );
+      },
+      tabBarActiveTintColor: '#7F5DF0',
+      tabBarInactiveTintColor: 'gray',
+      tabBarStyle: {
+        position: 'absolute',
+        bottom: 30,
+        left: 20,
+        right: 20,
+        elevation: 0,
+        backgroundColor: '#ffffff',
+        borderRadius: 100,
+        height:60,
+        shadowColor: '#7F5DF0',
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        borderWidth: 1,
+        borderColor: '#fff'
+      },
+      tabBarShowLabel: false,
+    })}
+  >
     <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="Schedule" component={ScheduleScreen} />
     <Tab.Screen name="Settings" component={SettingsScreen} />
