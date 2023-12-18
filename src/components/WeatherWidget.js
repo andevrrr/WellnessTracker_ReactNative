1e6ed39898ad53d02cb190153711b74ef269ab97
Anton Kucherenko <anton.kucherenko@tuni.fi>
2023-12-18 17:11:01 +0300

    weather widget styling

--- src/components/WeatherWidget.js
+++ src/components/WeatherWidget.js
@@ -1,33 +1,107 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image } from 'react-native';
-import { getWeatherData } from '../services/weatherService'; // You need to implement this function
+import { View, Text, ActivityIndicator, Image, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import { Dimensions } from 'react-native';
+import axios from 'axios';
 
 const WeatherWidget = () => {
   const [weather, setWeather] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
-    // Assume getWeatherData is an async function that fetches weather data
-    const fetchWeather = async () => {
-      const data = await getWeatherData();
-      setWeather(data);
-    };
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
 
-    fetchWeather();
+      let location = await Location.getCurrentPositionAsync({});
+      getWeatherData(location.coords.latitude, location.coords.longitude);
+    })();
   }, []);
 
+  const getWeatherData = async (latitude, longitude) => {
+    try {
+      const WEATHER_API_KEY = '4dd92c06b63e4535bc893519232211';
+      const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${latitude},${longitude}`);
+      const { temp_c, condition } = response.data.current;
+      const { name } = response.data.location;
+      setWeather({
+        temperature: temp_c,
+        city: name,
+        condition: condition.text
+      });
+    } catch (error) {
+      setErrorMsg('Failed to load weather data');
+      console.error(error);
+    }
+  };
+
+  if (errorMsg) {
+    return <Text>{errorMsg}</Text>;
+  }
+
+  if (!weather) {
+    return <ActivityIndicator size="large" />;
+  }
+
+  const screenWidth = Dimensions.get('window').width;
+  const widgetSize = (screenWidth / 2) - 20;
+
+const styles = StyleSheet.create({
+    widgetContainer: {
+      padding: 20,
+      borderRadius: 40,
+      backgroundColor: 'white',
+      width: widgetSize,
+      height: widgetSize,
+      marginRight: 10,
+      marginTop: 50,
+      shadowColor: '#7F5DF0',
+      shadowOffset: { width: 0, height: 5 },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.5,
+      elevation: 5,
+      justifyContent: 'center',
+    },
+    textContainer: {
+        width: '100%',
+      },
+    cityText: {
+      fontSize: 34,
+      fontWeight: 'bold',
+      color: 'black',
+    },
+    temperatureText: {
+      fontSize: 30,
+      color: 'black',
+    },
+    conditionImage: {
+      position: 'absolute',
+      top: 10,
+      right: 10,
+      width: 50,
+      height: 50,
+      borderColor: '#7F5DF0',
+      borderWidth: 2,
+      borderRadius: 25,
+    },
+  });
+
   return (
-    <View style={{ padding: 20, borderRadius: 10, backgroundColor: 'white' }}>
-      {weather ? (
-        <>
-          <Text>{`${weather.temperature}°`}</Text>
-          <Text>{weather.condition}</Text>
-          <Image source={{ uri: weather.iconUrl }} style={{ width: 50, height: 50 }} />
-        </>
-      ) : (
-        <Text>Loading...</Text>
-      )}
+    <View style={styles.widgetContainer}>
+      <View style={styles.textContainer}>
+        <Text style={styles.cityText}>{weather.city}</Text>
+        <Text style={styles.temperatureText}>{`${weather.temperature}°C`}</Text>
+      </View>
+      {/* <Image
+        style={styles.conditionImage}
+        source={{ uri: conditionImageSource }} // replace with your image source
+      /> */}
     </View>
   );
 };
 
 export default WeatherWidget;
+
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
 import WeatherWidget from '../components/WeatherWidget';
-import StepsWidget from '../components/StepsWidget';
+//import StepsWidget from '../components/StepsWidget';
 // import ScheduleWidget from '../components/ScheduleWidget';
 // import TasksWidget from '../components/TasksWidget';
 
 const HomeScreen = () => (
   <ScrollView style={{ flex: 1 }}>
-    <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 10 }}>
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 30 }}>
       <WeatherWidget />
-      <StepsWidget />
     </View>
   </ScrollView>
 );
